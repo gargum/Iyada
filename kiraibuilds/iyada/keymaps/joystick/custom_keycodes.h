94a63ab9dcd410216d86f5e48d3554a712dc18c3
Gareth <143119785+gargum@users.noreply.github.com>
2024-08-29 00:50:11 -0400

    Fixing broken joystick keycodes

--- kiraibuilds/iyada/keymaps/joystick/custom_keycodes.h
+++ kiraibuilds/iyada/keymaps/joystick/custom_keycodes.h
@@ -1,11 +1,7 @@
 // Remember, never put sensitive information like a password into this file!
-
+#include QMK_KEYBOARD_H
 //Joystick button keycodes - Simplified, more easily understood namings
-    #define GC_DPU JS_18
-	#define GC_DPD JS_16
-	#define GC_DPL JS_15
-	#define GC_DPR JS_17
-	#define GC_SQU JS_0
+    	#define GC_SQU JS_0
 	#define GC_CRO JS_1
 	#define GC_CIR JS_2
 	#define GC_TRI JS_3
@@ -18,3 +14,7 @@
 	#define GC_STA JS_10
 	#define GC_SEL JS_11
 	#define GC_HOM JS_12
+	#define GC_DPU JS_13
+	#define GC_DPD JS_14
+	#define GC_DPL JS_15
+	#define GC_DPR JS_16
