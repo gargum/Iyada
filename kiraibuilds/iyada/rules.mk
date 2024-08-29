da18c6e10a9f7fd071f9a682780787f0348ceef4
Gareth <143119785+gargum@users.noreply.github.com>
2024-08-28 23:37:57 -0400

    Adding custom rules.mk functions

--- kiraibuilds/iyada/rules.mk
+++ kiraibuilds/iyada/rules.mk
@@ -1,22 +1,43 @@
-# Microcontroller selection
-MCU = RP2040
+#
+# This file exists to configure the behaviour of the Iyada across all versions, keymaps, etc.
+#
 
+# Microcontroller selection
+	MCU = RP2040									# Selecting the Microcontroller used for the Iyada
+	
 # Bootloader selection
-BOOTLOADER = rp2040
+	BOOTLOADER = rp2040								# Selecting the appropriate bootloader for the above Microcontroller
+
+# Default path
+	DEFAULT_FOLDER = kiraibuilds/iyada/rev1			# Selecting the default subtype of Iyada to make if none are selected
 
 # Build Options
-SPLIT_KEYBOARD = yes							# Confirming that this is a split keyboard
-
-SERIAL_DRIVER = vendor
-LTO_ENABLE = yes								# Link Time Optimisation
-SLEEP_LED_ENABLE = yes							# LEDs inactive when computer is sleeping
-
-OS_DETECTION_ENABLE = no						# Operating System detection toggle
-
-POINTING_DEVICE_ENABLE = yes					# Mouse sensor input toggle
-POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c	# Mouse sensor model
-
-ENCODER_ENABLE = yes		  					# Rotary encoder toggle
-
-HAPTIC_ENABLE = yes								# Haptic feedback toggle
-HAPTIC_DRIVER = drv2605l                        # Haptic feedback driver
+	SPLIT_KEYBOARD = yes							# Confirming that this is a split keyboard
+	SERIAL_DRIVER = vendor
+	LTO_ENABLE = yes								# Link Time Optimization - This helps the firmware take up less space
+	
+# Tap Dance
+	TAP_DANCE_ENABLE = yes							# Tap Dance toggle
+	
+# Combos
+	COMBO_ENABLE = yes								# Combo toggle
+	
+# Cirque dual pointing functionality
+	POINTING_DEVICE_ENABLE = yes					# Cirque Pinnacle toggle
+	
+# Cirque dual analogue functionality
+	JOYSTICK_ENABLE = yes							# Cirque Pinnacle Gamepad toggle
+	
+# Rotary encoder options
+	ENCODER_ENABLE = yes		  					# Rotary encoder toggle
+	EC12_LEFT = yes									# Low-profile Rotary toggles
+	EC12_RIGHT = yes								# Select yes when using EC12 Rotary encoders and no when using EC11 Rotary encoders
+	
+# Per-key RGB lighting
+	RGBLIGHT_ENABLE = yes							# Per-key LED toggle
+	
+# OLED screens
+	OLED_ENABLE = yes								# OLED 32x128 display toggle
+	
+# Haptic feedbacK
+	HAPTIC_ENABLE = yes								# Haptic feedback toggle
