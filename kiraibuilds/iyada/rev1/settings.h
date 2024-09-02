d2bda837214f69dad8ab7eb141f575e9d7f85e13
Gareth <143119785+gargum@users.noreply.github.com>
2024-09-02 11:18:26 -0400

    Update settings.h to include explanations of what's going on

--- kiraibuilds/iyada/rev1/settings.h
+++ kiraibuilds/iyada/rev1/settings.h
@@ -1,26 +1,40 @@
-//This file exists for the sake of convenience, specifically in order to make creating user-generated keymaps with a large number of layers less difficult.
+//This file exists to allow keymaps to dynamically include or exclude various additional files based on rules.mk settings
 
 #include QMK_KEYBOARD_H
 
+//Layer Maps - Allows the user to define their layers in a separate file
+//Layer Maps are useful if you wish to have various files and features reference one's keymap layers.
 #if __has_include( "layers.h" ) && LAYER_MAP == yes
     #include "layers.h"
 #endif
 
+//Combo Maps - Allows the user to dynamically include or exclude a combos.def file without having to add or remove the associated #include line from keymap.c
 #if __has_include( "combos.def" ) && COMBO_MAP == yes
     #include "keyboards/gboards/g/keymap_combo.h"
 #endif
 
+//Tap Dance Maps - Allows the user to define Tap Dance functions in a separate file, making them easier to understand and easier to edit as a result.
 #if __has_include( "tap_dance_map.c" ) && TAP_DANCE_MAP == yes
     #include <tap_dance_map.c>
 #endif
 
+//Default Configurations - Allows the user to choose whether or not to include default settings like the Cirque joysticks and RGB layering
 #if DEFAULT_CONFIGURATIONS == yes
     #include "kiraibuilds/iyada/rev1/iyada_defaults.c"
 #endif
 
+//OS Detection - This is being used for the dynamic power draw code
 #ifdef OS_DETECTION_ENABLE
     #include <os_detection.h>
 
+/* Dynamic power draw:
+*
+*  When the keyboard boots, it draws little enough power to work across all platforms including iOS.
+*  When the keyboard detects the Operating System of the system it has been plugged into, it changes its own maximum power draw
+*  Power draw is left as-is if needed, but is increased as much as possible otherwise
+*
+*  NOTE - Mac OS has been given lower power consumption than Windows and Linux to ensure the Iyada plays nicely with Macbook laptops
+*/
 bool process_detected_host_os_kb(os_variant_t detected_os) {
     if (!process_detected_host_os_user(detected_os)) {
         return false;
