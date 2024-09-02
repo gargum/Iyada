66aba5c225b2850b4216ce02ddf60714bead7e99
Gareth <143119785+gargum@users.noreply.github.com>
2024-09-02 00:39:41 -0400

    Giving every keymap a layer map

---
+++ kiraibuilds/iyada/keymaps/joystick/layers.h
@@ -0,0 +1,7 @@
+//This file exists for the sake of convenience, specifically in order to make creating user-generated keymaps with a large number of layers less difficult.
+
+#include QMK_KEYBOARD_H
+
+// Keymap Layer Declarations
+//
+#define _GAMES 0
