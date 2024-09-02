189afacf994ed2766baabc7db8d7cd6412138ee2
Gareth <143119785+gargum@users.noreply.github.com>
2024-09-02 00:33:51 -0400

    Cleaning rules.mk

--- kiraibuilds/iyada/keymaps/default/rules.mk
+++ kiraibuilds/iyada/keymaps/default/rules.mk
@@ -1,26 +1,28 @@
 # Build Options
 COMBO_ENABLE = no								# Key combos
+COMBO_MAP = no
+
+CUSTOM_CODES = no							    # Custom keycode toggle
+UNICODE_ENABLE = no								# Unicode output
+WPM_ENABLE = no									# Word Per Minute measurement toggle
+
 TAP_DANCE_ENABLE = no							# Tap Dance toggle
-LEADER_ENABLE = no								# Leader key toggle
+TAP_DANCE_MAP = no								# Tap Dance definitions file toggle
+DYNAMIC_TAPPING_TERM_ENABLE = no				# Dynamic tapping term toggle
+
 AUTOCORRECT_ENABLE = no 						# Autocorrect toggle
 CAPS_WORD_ENABLE = no							# Caps word toggle
 AUTO_SHIFT_ENABLE = no							# Auto shift toggle
 DYNAMIC_MACRO_ENABLE = no						# Dynamic macro toggle
 KEY_LOCK_ENABLE = no							# Key lock toggle
-DYNAMIC_TAPPING_TERM_ENABLE = no				# Dynamic tapping term toggle
-WPM_ENABLE = no									# Word Per Minute measurement toggle
 
 STENO_ENABLE = no								# Stenotype output
-MIDI_ENABLE = no								# MIDI output
+
 AUDIO_ENABLE = no 								# Audio output
-UNICODE_ENABLE = no								# Unicode output
+MIDI_ENABLE = no								# MIDI output
+
 JOYSTICK_ENABLE = no 							# Gamepad & Joystick output
 JOYSTICK_DRIVER = digital					    # Gamepad & Joystick driver
-HAPTIC_DRIVER = drv2605l
-ENCODER_MAP_ENABLE = yes						# Encoder map toggle
-  
-POINTING_DEVICE_ENABLE = yes					# Mouse sensor input toggle
-POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c	# Mouse sensor model
-OLED_ENABLE = yes								# OLED display output toggle
-BACKLIGHT_ENABLE = no							# Keyboard backlight
-RGBLIGHT_ENABLE = yes							# Per-key LED toggle
+
+HAPTIC_ENABLE = no								# Haptic feedback toggle
+HAPTIC_DRIVER = drv2605l						# Haptic feedback driver
