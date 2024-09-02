6ecdc17f7f858a2d54e9afcc012c1df0bda4fb0b
Gareth <143119785+gargum@users.noreply.github.com>
2024-09-02 00:26:27 -0400

    Cleaning/Organising rules.mk

--- kiraibuilds/iyada/keymaps/advanced/rules.mk
+++ kiraibuilds/iyada/keymaps/advanced/rules.mk
@@ -1,29 +1,28 @@
 # Build Options
+COMBO_ENABLE = yes								# Key combo toggle
+COMBO_MAP = yes
+
+CUSTOM_CODES = yes							    # Custom keycode toggle
+UNICODE_ENABLE = no								# Unicode output
+WPM_ENABLE = no									# Word Per Minute measurement toggle
+
+TAP_DANCE_ENABLE = yes							# Tap Dance toggle
+TAP_DANCE_MAP = yes								# Tap Dance definitions file toggle
+DYNAMIC_TAPPING_TERM_ENABLE = no				# Dynamic tapping term toggle
+
 AUTOCORRECT_ENABLE = yes 						# Autocorrect toggle
 CAPS_WORD_ENABLE = yes							# Caps word toggle
 AUTO_SHIFT_ENABLE = yes							# Auto shift toggle
 DYNAMIC_MACRO_ENABLE = no						# Dynamic macro toggle
-DYNAMIC_TAPPING_TERM_ENABLE = no				# Dynamic tapping term toggle
-WPM_ENABLE = no									# Word Per Minute measurement toggle
+KEY_LOCK_ENABLE = no							# Key lock toggle
 
 STENO_ENABLE = yes								# Stenotype output
-MIDI_ENABLE = no								# MIDI output
+
 AUDIO_ENABLE = yes 								# Audio output
-UNICODE_ENABLE = no								# Unicode output
+MIDI_ENABLE = no								# MIDI output
 
-ifdef POINTING_DEVICE_ENABLE
-	POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c	# Mouse sensor model
-endif
+JOYSTICK_ENABLE = yes							# Gamepad & Joystick output
+JOYSTICK_DRIVER = digital					    # Gamepad & Joystick driver
 
-ifdef JOYSTICK_ENABLE
-	JOYSTICK_DRIVER = digital					    # Gamepad & Joystick driver
-endif
-
-ifdef RGBLIGHT_ENABLE
-	SLEEP_LED_ENABLE = yes							# LEDs inactive when computer is sleeping
-	SRC += layer_behaviours.c					    # Parsing the layer_behaviours.c file (RGB Light Layers, Layer sounds, Cirque Dual Joystick)
-endif					
-
-ifdef HAPTIC_ENABLE
-	HAPTIC_DRIVER = drv2605l                        # Haptic feedback driver
-endif
+HAPTIC_ENABLE = yes								# Haptic feedback toggle
+HAPTIC_DRIVER = drv2605l						# Haptic feedback driver
