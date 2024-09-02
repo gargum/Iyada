b5cb0563a16a412c294fe90022dab969c8d2a9e3
Gareth <143119785+gargum@users.noreply.github.com>
2024-09-02 00:16:16 -0400

    Adding CUSTOM_CODES rule support

--- kiraibuilds/iyada/iyada.h
+++ kiraibuilds/iyada/iyada.h
@@ -1,11 +1,16 @@
 /* 
-This file exists solely to allow the user to create and define a custom LED matrix or RGB matrix for the Iyada that uses a non-standard style of LED.
+* This file exists to make code in keymap.c files cleaner and more readable.
+* The file may also be used to allow users to create and define a custom LED matrix or RGB matrix for the Iyada using a non-standard style of LED.
 */
 
 #pragma once
 
-#include "quantum.h"
-#include <stdint.h>
-#include <stdbool.h>
+    #include "quantum.h"
+    #include <stdint.h>
+    #include <stdbool.h>
+    
+#if __has_include( <iyada/rev1/custom_keycodes.c> ) && CUSTOM_CODES == yes
+    #include <iyada/rev1/custom_keycodes.c>
+#endif
 
 void init_iyada(void);
